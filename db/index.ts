f2453103323e580fc103d0c6a4e8340504917463
Tiede Bakker <tiede.bakker@gmail.com>
2026-06-13 01:27:23 +0200

    dikis v 0.4

--- db/index.ts
+++ db/index.ts
@@ -1,3 +1,13 @@
+// import { drizzle } from "drizzle-orm/libsql";
+// import { createClient } from "@libsql/client";
+// import * as schema from "./schema";
+
+// const client = createClient({
+//   url: process.env.TURSO_CONNECTION_URL!,
+//   authToken: process.env.TURSO_AUTH_TOKEN,
+// });
+
+// export const db = drizzle(client, { schema });
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 import * as schema from "./schema";
@@ -5,6 +15,14 @@ import * as schema from "./schema";
 const client = createClient({
   url: process.env.TURSO_CONNECTION_URL!,
   authToken: process.env.TURSO_AUTH_TOKEN,
+  
+  // TOEVOEGEN: De ultieme Next.js cache-sloper voor Turso
+  fetch: (input, init) => {
+    return fetch(input, {
+      ...init,
+      cache: "no-store", // Dit dwingt Vercel om ELKE SQL-query live uit te voeren
+    });
+  },
 });
 
 export const db = drizzle(client, { schema });
